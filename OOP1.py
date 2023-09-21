864c1e2044848b9589f18180646021270da5c360
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-09-21 13:46:56 +0300

    Add files via upload

---
+++ OOP1.py
@@ -0,0 +1,24 @@
+Name = input("Enter your name:")
+Course = input("Enter your course:")
+Unit = input("Enter your unit:")
+Reg_no = input("Enter your registration number: ")
+Department = input("Enter your department:")
+
+Faculty = input("Enter your faculty:")
+Email = input("Enter your email:")
+Contact = input("Enter your phone number:")
+
+
+"""print("Name:",Name)
+print("Course:",Course)
+print("Unit:",Unit)
+
+print("Reg_no:",Reg_no)
+print("Department:",Department)
+print("Faculty:",Faculty)
+print("Email:",Email)
+print("Contact:",Contact)
+"""
+print(Name, Course, Unit, Reg_no, Department, Faculty, Email, Contact)
+
+
