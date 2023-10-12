98e66a868d5fd429d4daf952e121d8467a5213f1
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-10-12 21:51:47 +0300

    Add files via upload

---
+++ OOP7.py
@@ -0,0 +1,21 @@
+# class student():
+#     Name = "name"
+#     Access_no = "access"
+#     Reg_no = "reg"
+#     course = "course"
+#     yearofbirth =    2000
+
+
+# def student_age(self, yearofbirth):
+#     self.age = 2023 - yearofbirth
+
+
+
+
+
+
+# print(f"my age is. {self.age}")
+
+print("hell0 world")
+
+
