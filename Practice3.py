30b7134be1ce289d24a5ae482350a7ecd524ee59
Amos Mugabi <115138015+Amos-Mugabi@users.noreply.github.com>
2023-11-09 20:25:24 +0300

    Add files via upload

---
+++ Practice3.py
@@ -0,0 +1,11 @@
+# Functions:
+def name():
+    f_name = input("Enter your first name:")
+    l_name = input("Enter your last name:")
+
+
+    print ("Your name is: ", f_name.upper(), l_name.upper())          
+
+name()
+
+
